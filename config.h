94ee140033d8a32dcc04ab75313e05ad52fc848f
Bspeake <benjaminspeake@gmail.com>
2023-04-04 15:53:44 +0100

    Initial commit

---
+++ config.h
@@ -0,0 +1,11 @@
+#pragma once 
+
+#include <iostream>
+#include <vector>
+#include <array>
+#include <sstream>
+#include <fstream>
+#include <string>
+
+#include <glad/glad.h> 
+#include <GLFW/glfw3.h>
---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include "config.h"
+
+int main() {
+	int w = 640;
+	int h = 480;
+
+}
